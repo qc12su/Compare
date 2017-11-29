8906c5792cebeb2bbbf0c8bbab9b1c092439817d
Randy <randycchen@gmail.com>
2017-11-29 18:22:04 -0500

    Register User - send json string to /addUser

--- Compare/src/Components/LoginPage/Register.js
+++ Compare/src/Components/LoginPage/Register.js
@@ -8,6 +8,21 @@ const rowStyle = {
      margin: "5px", 
      verticalAlign: "center"
 }
+
+function addUser(data) {
+                console.log("called add User");
+                 return fetch('http://localhost:3000/addUser', {
+                     method: 'POST',
+                     mode: 'CORS',
+                     body: JSON.stringify(data),
+                     headers: {
+                         'Content-Type': 'application/json'
+                     }
+                 }).then(res => {
+                     return res;
+                 }).catch(err => err);
+}
+
 export default class Register extends React.Component {
     constructor(props) {
         super(props);
@@ -36,6 +51,7 @@ export default class Register extends React.Component {
             password: this.state.password,
             email: this.state.email
         }
+        addUser(data);
         alert('Hello ' + data.username + ', your password is: ' + data.password + ' and  your email is ' + data.email);
         event.preventDefault();
     }
@@ -70,39 +86,33 @@ export default class Register extends React.Component {
                 <Dialog title="Register" style = {{textAlign:"left"}} actions={actions} modal={true} open={this.state.open}>
                      <form>
                         <Grid fluid ={true}>
-                            <Row style ={rowStyle}  >
-                                <div class="col-md-2 col-md-offset-3">
-                                Username:
-                                </div>
+                            <Row style ={rowStyle}>
+                                <div class = "col-md-2 col-md-offset-3">
+                                Username: </div>
                                 <div class="col-md-2">
-                                    <input value={this.state.username} onChange= {this.handleChange('username')} />
+                                    <input path = "username" value={this.state.username} onChange= {this.handleChange('username')} />
                                 </div>
-                                
                             </Row>
                             <Row style ={rowStyle}>
                                 <div class="col-md-2 col-md-offset-3">
-                                Password:
-                                </div>
+                                Password: </div>
                                 <div class="col-md-2">
-                                    <input type="password"
+                                    <input type="password" path = "password"
                                     value={this.state.password} 
                                     onChange= {this.handleChange('password')}/>
                                 </div>
                             </Row>
                             <Row style ={rowStyle}>
                                 <div class="col-md-2 col-md-offset-3">
-                                Email:
-                                </div>
+                                Email: </div>
                                 <div class="col-md-2">
-                                    <input 
+                                    <input
+                                    path = "email"
                                     value={this.state.email} 
                                     onChange= {this.handleChange('email')} />
                                 </div>
                             </Row>
                         </Grid>
-                        
-                            
-                        
                       </form>
                 </Dialog>
             </div>
